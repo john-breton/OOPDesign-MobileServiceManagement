8821ae93139726889e26595d61d8bc84ef334d6e
John Breton <55368611+john-breton@users.noreply.github.com>
2020-12-23 14:59:22 -0500

    Add method stubs
    - Added method stubs per the generated UML diagram. These methods require implementation of the datatypes User, Account, and Bundle. This will be completed once the work from the BundleManagement class is merged into main.
    - An additional change was to reimplement the Singleton pattern to use eager instantion. This was done since the AccountManagementclass is always needed and the overhead associated with its instantiation will not have a major impact on startup.

--- src/main/java/AccountManagement/AccountManagement.java
+++ src/main/java/AccountManagement/AccountManagement.java
@@ -1,18 +1,86 @@
 package AccountManagement;
 
+
+/**
+ * The AccountManagement class is responsible for managing accounts
+ * that belong to the overall system. An Account is composed of a User,
+ * a Bundle, and a phone number.
+ *
+ * The AccountManagement class will maintain a list of Accounts in order
+ * to perform management operations such as adding, deleting, or modifying
+ * an existing account.
+ *
+ * AccountManagement is a Singleton. This means that the class contains a
+ * private constructor and ensures that only one instance of the class will
+ * exist at any given time. This is to prevent data corruption once it comes
+ * to multiple admins making use of the AccountManagement service.
+ */
 public class AccountManagement {
-	private static AccountManagement uniqueInstance;
-	
-	// other instance variables
-	
+	// We will always need an AccountManagement instance, so use eager instantiation.
+	private static AccountManagement uniqueInstance = new AccountManagement();
+
 	private AccountManagement() {}
-	
-	public static synchronized AccountManagement getInstance() {
-		if(uniqueInstance == null) {
-			uniqueInstance = new AccountManagement();
-		}
+
+	/**
+	 * Get the AccountManagement instance.
+	 *
+	 * @return The Singleton instance of the AccountManagement class.
+	 */
+	public static AccountManagement getInstance() {
 		return uniqueInstance;
 	}
-	
-	// other methods
+
+	/**
+	 *
+	 * @param user
+	 * @param phoneNum
+	 * @param bundle
+	 */
+	public void addAccount(User user, String phoneNum, Bundle bundle) {
+		return;
+	}
+
+	/**
+	 *
+	 * @param acc
+	 */
+	public void addAccount(Account acc) {
+		return;
+	}
+
+	/**
+	 *
+	 * @param phoneNum
+	 */
+	public void removeAccount(String phoneNum) {
+		return;
+	}
+
+	/**
+	 *
+	 * @param phoneNum
+	 * @param bundle
+	 */
+	public void updateAccount(String phoneNum, Bunble bundle) {
+		return;
+	}
+
+	/**
+	 *
+	 * @param phoneNum
+	 * @return
+	 */
+	public Account getAccount(String phoneNum) {
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param phoneNum
+	 * @param bundle
+	 */
+	public void updateBundle(String phoneNum, Bundle bundle) {
+		return;
+	}
+
 }
